d81bfb3fb8f9e4e83bdfb0abc490b8c22a0a5b7c
ArmandoHub <104286442+ArmandoHub@users.noreply.github.com>
2026-06-13 20:43:53 -0600

    Actualizar NavMenu.tsx

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -68,6 +68,13 @@ export default function NavMenu() {
               Ranking
             </Link>
 <Link
+  href="/rankingTest"
+  onClick={() => setOpen(false)}
+  className="block px-4 py-2 text-sm text-slate-700 hover:bg-slate-50"
+>
+  Historial de Resultados
+</Link>
+<Link
   href="/resultados"
   onClick={() => setOpen(false)}
   className="block px-4 py-2 text-sm text-slate-700 hover:bg-slate-50"
